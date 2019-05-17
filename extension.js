640638078d3e053415f8115bc9aa3f3ba5fec67c
Jeff Hykin <jeff.hykin@gmail.com>
2019-05-16 19:09:07 -0500

    add command for exectuing by name

--- extension.js
+++ extension.js
@@ -1,15 +1,16 @@
 const { window } = require("vscode")
 const vscode = require("vscode")
 
-var activeContext
-var disposables = []
-
+let activeContext
+let disposables = []
+let macros = {}
+let invalidMacroNames = [ "has", "get", "update", "inspect" ]
 exports.activate = function activate(context) {
     loadMacros(context)
     activeContext = context
     vscode.workspace.onDidChangeConfiguration(() => {
         // dispose of macros
-        for (var disposable of disposables) {
+        for (let disposable of disposables) {
             disposable.dispose()
         }
         // reload them
@@ -19,67 +20,76 @@ exports.activate = function activate(context) {
 
 exports.deactivate = function deactivate() {}
 
+async function executeMacro(name) {
+    // iterate over every action in the macro
+    for (const action of macros[name]) {
+        // if its a string assume its a command
+        if (typeof action == "string") {
+            await vscode.commands.executeCommand(action)
+            // otherwise check if its an object
+        } else if (action instanceof Object) {
+            //
+            // Check if its a javascript macro
+            //
+            if (typeof action.javascript == "string") {
+                eval(action.javascript)
+                continue
+            }
+            //
+            // Check for injections
+            //
+            let replacements = []
+            let actionCopy = JSON.parse(JSON.stringify(action))
+            if (action.injections) {
+                for (let eachInjection of action.injections) {
+                    //
+                    // Compute the value the user provided
+                    //
+                    let value = eval(eachInjection.withResultOf)
+                    if (value instanceof Promise) {
+                        value = await value
+                    }
+                    value = `${value}`
+                    //
+                    // replace it in the arguments
+                    //
+                    for (let eachKey in actionCopy.args) {
+                        // if its a string value, then perform a replacement
+                        // TODO, this is currently shallow, it should probably be recursive
+                        if (typeof actionCopy.args[eachKey] == "string") {
+                            actionCopy.args[eachKey] = actionCopy.args[eachKey].replace(eachInjection.replace, value)
+                        }
+                    }
+                }
+            }
+            //
+            // run the command
+            //
+            await vscode.commands.executeCommand(actionCopy.command, actionCopy.args)
+        }
+    }
+}
+
 function loadMacros(context) {
     // get the macros from the settings file
-    const macros = vscode.workspace.getConfiguration("macros")
+    macros = vscode.workspace.getConfiguration("macros")
 
     // look at each macro
     for (const name in macros) {
-        // skip the built-in ones
-        if (name == "has" || name == "get" || name == "update") {
+        // skip the things that are not arrays
+        if (!(macros[name] instanceof Array)) {
             continue
         }
         // register each one as a command
-        const disposable = vscode.commands.registerCommand(`macros.${name}`, async function() {
-            // iterate over each action
-            for (const action of macros[name]) {
-                // if its a string assume its a command
-                if (typeof action == "string") {
-                    await vscode.commands.executeCommand(action)
-                    // otherwise check if its an object
-                } else if (action instanceof Object) {
-                    //
-                    // Check if its a javascript macro
-                    //
-                    if (typeof action.javascript == "string") {
-                        eval(action.javascript)
-                        continue
-                    }
-                    //
-                    // Check for injections
-                    //
-                    let replacements = []
-                    let actionCopy = JSON.parse(JSON.stringify(action))
-                    if (action.injections) {
-                        for (let eachInjection of action.injections) {
-                            //
-                            // Compute the value the user provided
-                            //
-                            let value = eval(eachInjection.withResultOf)
-                            if (value instanceof Promise) {
-                                value = await value
-                            }
-                            value = `${value}`
-                            //
-                            // replace it in the arguments
-                            //
-                            for (let eachKey in actionCopy.args) {
-                                // if its a string value, then perform a replacement
-                                // TODO, this is currently shallow, it should probably be recursive
-                                if (typeof actionCopy.args[eachKey] == "string") {
-                                    actionCopy.args[eachKey] = actionCopy.args[eachKey].replace(eachInjection.replace, value)
-                                }
-                            }
-                        }
-                    }
-                    //
-                    // run the command
-                    //
-                    await vscode.commands.executeCommand(actionCopy.command, actionCopy.args)
-                }
-            }
-        })
+        const disposable = vscode.commands.registerCommand(`macros.command.${name}`, ()=>executeMacro(name))
         context.subscriptions.push(disposable)
         disposables.push(disposable)
     }
 }
+
+// create a command for running macros by name
+vscode.commands.registerCommand('macro.run', async () => {
+    let macroNames = Object.keys(macros).filter(each=>macros[each] instanceof Array)
+    let result = await window.showQuickPick(macroNames)
+    executeMacro(result)
+})
