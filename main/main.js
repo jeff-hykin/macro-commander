f066de31866ec19fa1843ddcf05f594d2b7f1b27
Jeff Hykin <jeff.hykin@gmail.com>
2023-12-16 18:15:04 -0500

    fix missing import

--- main/main.js
+++ main/main.js
@@ -2,6 +2,7 @@ const { window } = require("vscode")
 const vscode = require("vscode")
 const { execSync } = require("child_process")
 const path = require("path")
+const fs = require("fs")
 
 // 
 // globals
